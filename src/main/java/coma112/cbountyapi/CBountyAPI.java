cda51a5b1fed0cbac933189ba5b40094f19331d3
Coma112 <lolkd005@gmail.com>
2024-06-05 17:49:53 +0200

    v2.0

--- src/main/java/coma112/cbountyapi/CBountyAPI.java
+++ src/main/java/coma112/cbountyapi/CBountyAPI.java
@@ -1,16 +1,17 @@
 package coma112.cbountyapi;
 
+import coma112.cbounty.CBounty;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CBountyAPI extends JavaPlugin {
+    @Getter private static CBounty cbounty;
+    @Getter private static CBountyAPI instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-    }
+        instance = this;
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        cbounty = new CBounty();
     }
 }
